6ecf48a37110bba13ba6ba3eb5604d71aa6d7937
Vladimir Akulenko <vladimirakulenk@yandex.ru>
2024-06-01 22:22:06 +0300

    hw jpa-repository-query

--- src/main/java/ru/netology/repository/Repository.java
+++ src/main/java/ru/netology/repository/Repository.java
@@ -2,6 +2,8 @@ package ru.netology.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.netology.entity.Person;
 import ru.netology.entity.PersonId;
 
@@ -10,9 +12,12 @@ import java.util.Optional;
 
 @org.springframework.stereotype.Repository
 public interface Repository extends JpaRepository<Person, PersonId> {
-    List<Person> findByCityOfLiving(String city);
+    @Query(value = "select p from Person p where p.cityOfLiving = :city")
+    List<Person> findByCityOfLiving(@Param("city") String city);
 
-    List<Person> findByAgeLessThanOrderByAge(int age);
+    @Query(value = "select p from Person p where p.age < :age order by p.age")
+    List<Person> findByAgeLessThanOrderByAge(@Param("age") int age);
 
-    Optional<Person> findByNameAndSurname(String name, String surname);
+    @Query(value = "select p from Person p where p.name = :name and p.surname = :surname")
+    Optional<Person> findByNameAndSurname(@Param("name") String name, @Param("surname") String surname);
 }
